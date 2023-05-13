fadb77967ce805c6c7865ab3d33c3e56084d6e01
Ibrahim seh <ibrahim-seh@outlook.com>
2023-05-13 18:06:07 +0300

    ...

--- client/src/components/Card/CardEdit.jsx
+++ client/src/components/Card/CardEdit.jsx
@@ -1,10 +1,133 @@
-import { Typography } from "@mui/material";
+import { Alert, Box, Container, Grid, Typography } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+import ROUTES from "../../routes/ROUTES";
+import validateCardSchema from "../../validation/CreateCardValidation";
+import SubmitComponent from "../Form/SubmitComponent";
+import CRComponent from "../Form/CRComponent";
+import GridItemComponent from "../Form/GridItemComponent";
+import useQueryParams from "../../hooks/useQueryParams";
 
 const CardEdit = () => {
+  let qparams = useQueryParams();
+  console.log("qparams.filter = " + qparams.filter);
+
+  const [inputState, setInputState] = useState({
+    title: "",
+    subTitle: "",
+    description: "",
+    state: "",
+    country: "",
+    city: "",
+    street: "",
+    houseNumber: "",
+    zipCode: "",
+    phone: "",
+    email: "",
+    web: "",
+    url: "",
+    alt: "",
+  });
+
+  const [inputsErrorsState, setInputsErrorsState] = useState(null);
+  const navigate = useNavigate();
+
+  const handleBtnSubmitClick = async (ev) => {
+    try {
+      const joiResponse = validateCardSchema(inputState);
+      setInputsErrorsState(joiResponse);
+
+      if (joiResponse) {
+        console.log("return from joiResponse");
+        return;
+      }
+      await axios.put("/cards/:id", {
+        title: inputState.title,
+        subTitle: inputState.subTitle,
+        description: inputState.description,
+        state: inputState.state,
+        country: inputState.country,
+        city: inputState.city,
+        street: inputState.street,
+        houseNumber: inputState.houseNumber,
+        email: inputState.email,
+        zipCode: inputState.zipCode,
+        phone: inputState.phone,
+        web: inputState.web,
+        url: inputState.url,
+        alt: inputState.alt,
+      });
+
+      toast.success("A new card has been created");
+      navigate(ROUTES.MYCARDS);
+    } catch (err) {
+      console.log("error from axios", err.response.data);
+      toast.error("the card has been not created");
+    }
+  };
+
+  const handleBtnCancelClick = () => {
+    navigate(ROUTES.MYCARDS);
+  };
+
+  const handleBtnResetClick = () => {
+    window.location.reload();
+  };
+
+  const updateState = (key, value) => {
+    inputState[key] = value;
+    console.log("inputState = ", inputState);
+  };
+
   return (
-    <Typography variant="h5" color="initial">
-      Card Edit Page
-    </Typography>
+    <Container component="main" maxWidth="md">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          CARD UPDATE
+        </Typography>
+
+        <Box component="div" noValidate sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            {Object.entries(inputState).map(([key, value]) => (
+              <Grid item xs={12} sm={6} key={Math.random() + Date.now()}>
+                <GridItemComponent
+                  inputKey={key}
+                  inputValue={value}
+                  onChange={updateState}
+                />
+                {inputsErrorsState && inputsErrorsState[key] && (
+                  <Alert severity="warning">
+                    {inputsErrorsState[key].map((item) => (
+                      <div key={`${key}-errors` + item}>
+                        {item.includes("pattern:")
+                          ? item.split("pattern:")[0] + "pattern"
+                          : item}
+                      </div>
+                    ))}
+                  </Alert>
+                )}
+              </Grid>
+            ))}
+            <CRComponent
+              cancelBtn={handleBtnCancelClick}
+              resetBtn={handleBtnResetClick}
+            />
+          </Grid>
+
+          <SubmitComponent onClick={handleBtnSubmitClick} />
+        </Box>
+      </Box>
+    </Container>
   );
 };
 export default CardEdit;
